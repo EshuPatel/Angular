24d222aa83e1fb17300165cc3441cafec3edba5a
Eshu Patel <138328728+EshuPatel@users.noreply.github.com>
2025-10-08 16:14:56 +0530

    Add files via upload

---
+++ upload/src/app/components/products/products.component.ts
@@ -0,0 +1,82 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../../model/product';
+import { ProductsService } from '../../service/products.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-products',
+  imports: [CommonModule],
+  templateUrl: './products.component.html',
+  styleUrl: './products.component.css'
+})
+export class ProductsComponent {
+
+  //observable is a type to handle async data streams
+  products: Observable<Product[]> | any;
+  message: string ='';
+
+  constructor(private productService: ProductsService,  private router: Router){
+
+  }
+
+
+  //angular life cycle mthds ngOnInit() - invoked when cpmonent initialised
+  ngOnInit(): void{
+    this.reloadData();
+  }
+
+
+
+
+  
+  reloadData(){
+    this.productService.getProductList().subscribe({
+      next: (data) =>{
+        this.products=data;  //assign the plain data, no need for async pipe in the template
+      },
+      error: (err) =>{
+        console.error('Error fetching product list: ', err.message)
+      }
+    });
+  }
+
+
+
+
+
+  addProduct(): void{
+    this.router.navigate(['/add-product/_add']);
+  }
+
+
+
+   editProduct(pid:number):void{
+    this.router.navigate(['/add-product', pid]);
+  }
+              
+  productDetails(pid:number):void{
+    this.router.navigate(['/product-details', pid]);
+  }
+
+
+
+  deleteProduct(pid:number):void{
+    this.productService.deleteProduct(pid).subscribe({
+      next: () => {
+        this.message = 'Product deleted successfully.';
+        setTimeout(() => {
+          this.message = '';
+          this.reloadData();  // Refresh products list after deletion
+        }, 2000);  // Clear the message after 2 seconds
+      },
+      error: (err) => {
+        console.error('Error deleting product:', err.message);  // Handle the error
+        this.message = 'Error deleting product. Please try again later.';
+      }
+    });
+  }
+
+  
+}
---
+++ upload/src/app/model/product.ts
@@ -0,0 +1,10 @@
+export class Product {
+     pid: number | any | null;
+    name: string | any;
+    brand: string | any;
+    madein: string | any;
+    price:number | any;
+    active: boolean | any;
+
+    
+}
